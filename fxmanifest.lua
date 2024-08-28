67213215e854bc1384e70e0516b1cbdb20efdda5
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2024-08-27 21:55:48 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Popcorn Roleplay and Partners - https://discord.gg/popcornroleplay'
+description 'A Simple Grapeseed Clinic MLO by Popcorn RP - https://discord.gg/popcornroleplay'
